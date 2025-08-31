a6ffcd4f2712669352c22f60b3c4612041c1acf3
cesarsouza13 <cesardanielmg@hotmail.com>
2025-08-31 18:52:19 -0300

    fix: refactor de api exception

--- src/main/java/application/grid/domain/exception/ApiException.java
+++ src/main/java/application/grid/domain/exception/ApiException.java
@@ -1,23 +1,9 @@
 package application.grid.domain.exception;
 
-import org.springframework.http.HttpStatus;
-
 public abstract class ApiException extends RuntimeException {
 
-    private final HttpStatus httpStatus;
-    private final String error;
-
-    public ApiException(String message, HttpStatus httpStatus, String error) {
-        super(message);
-        this.httpStatus = httpStatus;
-        this.error = error;
+    public ApiException(String mensagem) {
+        super(mensagem);
     }
 
-    public HttpStatus getHttpStatus() {
-        return httpStatus;
-    }
-
-    public String getError() {
-        return error;
-    }
 }
